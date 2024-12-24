1743aae13839c44a7a93ad19723f203e8ebdfa62
Tejas Junghare <tejasjunghare09@gmail.com>
2024-12-24 16:19:12 +0530

    tesst

--- CDAC_Project/tourmanagmentfrontend/src/screens/Home.jsx
+++ CDAC_Project/tourmanagmentfrontend/src/screens/Home.jsx
@@ -1,13 +1,9 @@
-
-
 function Home() {
-    return (
-        <div>
-            
-
-
-        </div>
-      );
+  return (
+    <div>
+      <h1>HELLOOO</h1>
+    </div>
+  );
 }
 
 export default Home;

75fbf3772ad67e92e6ee28cf40e09b58f7f0014f
Srushti-Jagtap <srushtijagtap001@gmail.com>
2025-01-29 17:04:30 +0530

    Update Profile

--- CDAC_Project/tourmanagmentfrontend/src/App.js
+++ CDAC_Project/tourmanagmentfrontend/src/App.js
@@ -14,6 +14,7 @@ import PurchasePackage from "./screens/PurchasePackage";
 import PaymentSuccess from "./screens/PaymentSuccess";
 import ContactUs from "./screens/ContactUs";
 import AboutUs from "./screens/AboutUs";
+import UpdateProfile from "./screens/UpdateProfile";
 
 function App() {
   return (
@@ -32,7 +33,8 @@ function App() {
         <Route path="/ContactUs" element={<ContactUs />} />
         <Route path="/AboutUs" element={<AboutUs />} />
         <Route path="/tour-details/:id" element={<TourDetails />} />
-        
+        <Route path="/UpdateProfile" element={<UpdateProfile />} />
+
       </Routes>
 
       <ToastContainer />

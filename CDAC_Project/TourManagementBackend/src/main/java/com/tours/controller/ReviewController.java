3c0582523a7913ea995b2ed9f7b1846b8acb3486
Srushti-Jagtap <srushtijagtap001@gmail.com>
2025-01-30 12:33:59 +0530

    Review Backend

---
+++ CDAC_Project/TourManagementBackend/src/main/java/com/tours/controller/ReviewController.java
@@ -0,0 +1,36 @@
+package com.tours.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.tours.dto.ReviewDTO;
+import com.tours.service.ReviewService;
+
+import jakarta.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+    
+    @Autowired
+    private ReviewService reviewService;
+
+    // Any user should be able to view all reviews
+    @GetMapping("/view")
+    public ResponseEntity<List<?>> viewReviews() {
+        return ResponseEntity.ok(reviewService.getAllReviews());
+    }
+
+   
+    // Add a review (only authenticated users should be able to add a review)
+    @PostMapping("/add")
+    public ResponseEntity<?> addReview(@RequestBody @Valid ReviewDTO reviewDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(reviewService.addReview(reviewDTO));
+    }
+
+   
+}
---
+++ CDAC_Project/TourManagementBackend/src/main/java/com/tours/service/ReviewServiceImpl.java
@@ -0,0 +1,41 @@
+package com.tours.service;
+
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tours.dao.ReviewDao;
+import com.tours.dto.ApiResponse;
+import com.tours.dto.ReviewDTO;
+import com.tours.entity.Review;
+
+
+@Service
+@Transactional
+public class ReviewServiceImpl implements ReviewService {
+
+    @Autowired
+    private ReviewDao reviewDao;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public ApiResponse addReview(ReviewDTO reviewDTO) {
+        Review review = mapper.map(reviewDTO, Review.class);
+        reviewDao.save(review);
+        return new ApiResponse("Added new review with ID " + review.getId());
+    }
+
+    @Override
+    public List<Review> getAllReviews() {
+        return reviewDao.findAll();
+    }
+
+   
+    
+}
